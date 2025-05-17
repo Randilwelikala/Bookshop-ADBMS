d44b8d5df1181ae40ebab56c428ea2f43f4983e6
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2025-05-17 17:15:36 +0530

    Completed Navbar.jsx and css

--- bookstore/src/Components/Navbar/Navbar.jsx
+++ bookstore/src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,67 @@
-import './Navbar.css';
-import { FaShoppingCart, FaUser } from "react-icons/fa";
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FaShoppingBag, FaUserCircle, FaBookOpen, FaLightbulb, FaGraduationCap } from "react-icons/fa";
 
-export default function Navbar() {
+const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        📚 BookStore
-      </div>
+      <div className="logo">BookNest</div>
 
-      <div className="navbar-links">
-        <a href="#">Home</a>
-        <a href="#">Books</a>
-      </div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
 
-      <div className="navbar-icons">
-        <button>
-          <FaShoppingCart />
-        </button>
-        <button>
-          <FaUser />
-        </button>
+        <li className="dropdown">
+          <a 
+            href="#"
+            className="dropbtn"
+            onClick={(e) => {
+              e.preventDefault();  
+              toggleDropdown();
+            }}
+          >
+            Categories ▾
+          </a>
+
+          {dropdownOpen && (
+            <ul className="dropdown-content" onClick={() => setDropdownOpen(false)}>
+              <li>
+                <a href="#">
+                  
+                  Fiction
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                   
+                  Non-fiction
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                   
+                  Academic
+                </a>
+              </li>
+            </ul>
+          )}
+        </li>
+
+        <li><a href="#">Best Sellers</a></li>
+        <li><a href="#">New Arrivals</a></li>
+      </ul>
+
+      <div className="right-icons">
+        <input type="text" placeholder="Search books..." />
+        <a href="#"><FaShoppingBag className="icon" /></a>
+        <a href="#"><FaUserCircle className="icon" /></a>
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
