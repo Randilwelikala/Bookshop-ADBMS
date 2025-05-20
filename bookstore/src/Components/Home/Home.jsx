3cb3029abdcfe9d28142c79044834c7ab72b36fa
SNimsara <nimsaragunasekara123@gmail.com>
2025-05-20 13:46:06 +0530

    home section 2

--- bookstore/src/Components/Home/Home.jsx
+++ bookstore/src/Components/Home/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Home.css";
+import home2 from "../../assets/img/home2.jpg";
+import home3 from "../../assets/img/home3.jpg";
+import home4 from "../../assets/img/home4.jpg";
+import home5 from "../../assets/img/home5.jpg";
+import home6 from "../../assets/img/home6.jpg";
 
 const Home = () => {
   return (
@@ -13,6 +18,36 @@ const Home = () => {
         </div>
         <div className="h-sec1-imge">
           <img src={""} alt="" />
+        </div>
+      </div>
+
+      <div className="h-sec2">
+        <h1>Book Categories</h1>
+        <div className="h-sec2-imge">
+          <div className="h-sec2-imge1">
+            <img src={home6} alt="" />
+            <h3>Educational</h3>
+          </div>
+          <div className="h-sec2-imge2">
+            <img src={home2} alt="" />
+            <h3>Children</h3>
+          </div>
+          <div className="h-sec2-imge3">
+            <img src={home4} alt="" />
+            <h3>Fiction</h3>
+          </div>
+          <div className="h-sec2-imge4">
+            <img src={home3} alt="" />
+            <h3>Novel</h3>
+          </div>
+          <div className="h-sec2-imge5">
+            <img src={home5} alt="" />
+            <h3>Short Story</h3>
+          </div>
+          <div className="h-sec2-imge6">
+            <img src={""} alt="" />
+            <h3>Language Learning</h3>
+          </div>
         </div>
       </div>
     </div>
