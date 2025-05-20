f31e9185ee9c6609f354ffb67d8c23dbcb98cb9a
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2025-05-20 10:27:14 +0530

    Children page

--- bookstore/src/App.jsx
+++ bookstore/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Login from './Components/Login/Login';
 import Cart from './Components/Cart/Cart';
+import Footer from './Components/Footer/Footer';
+import Children from './Components/Children/Children';
 
 const App = () => {
   return (
@@ -11,8 +13,11 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
+
+        <Route path="/children" element={<Children />} />
         {/* Add more routes as needed */}
       </Routes>
+      <Footer/>
     </Router>
   );
 };
